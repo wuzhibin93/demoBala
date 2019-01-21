566f268c773459402504b5e7179b02edfdc72035
吴志彬 <wuzhibinoo@163.com>
2019-01-21 16:15:44 +0800

    1.octopus实体类

---
+++ src/main/java/com/enlink/entity/Address_octopus.java
@@ -0,0 +1,19 @@
+package com.enlink.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @ClassName :
+ * @Author Aprwu
+ * @Description : TODO(ocotpus测试的实体类)
+ * @Date : Created in 14:02 2019/1/19
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address_octopus {
+    private String city;
+    private String detail;
+}
